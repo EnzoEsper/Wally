c31962e3c963b80e0600f6942961f8aedcaf14f9
enzoesper <enzoesper@hotmail.com>
2019-03-25 01:25:01 -0300

    se crean las functions contructors de los ingresos y gastos (y la estructura de datos p/ almacenarlos)

--- app.js
+++ app.js
@@ -2,6 +2,31 @@
 // CONTROLADOR BUDGET (DATA)
 var budgetController = (function(){
   
+  // function constructors para los ingresos y los gastos
+  var Expense = function(id, description, value){
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function(id, description, value){
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  // estructura de datos para almacenar ingresos y datos
+  var data = {
+    allItems: {
+      exp : [],
+      inc : []
+    },
+    totals : {
+      exp : 0 ,
+      inc : 0
+    }
+  }
+
 }());
 
 // CONTROLADOR UI
