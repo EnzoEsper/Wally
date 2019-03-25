4197df882c28571d7500a72cceee685bc3eaaafe
enzoesper <enzoesper@hotmail.com>
2019-03-25 00:25:26 -0300

    Leer datos de los input fields

--- app.js
+++ app.js
@@ -1,31 +1,55 @@
 
+// CONTROLADOR BUDGET (DATA)
 var budgetController = (function(){
-  var x = 33;
-
-  var add = function(a){
-    return(x + a);
-  }
-
-  return {
-    publicTest : function(b){
-      return add(b);
-    }
-  }
+  
 }());
 
+// CONTROLADOR UI
 var UIController = (function(){
 
-  // codigo
+  // Objeto con strings del DOM para facilitar el cambio de nombre de clases o ID's, si se llegara a necesitar en un futuro.
+  var DOMstrings = {
+    inputType : '.add__type' ,
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+
+  return {
+    getInput : function(){
+      return {
+        //se reemplazan los parametros con los strings del DOM corresp
+        type : document.querySelector(DOMstrings.inputType).value,
+        description : document.querySelector(DOMstrings.inputDescription).value,
+        value : document.querySelector(DOMstrings.inputValue).value
+      }
+    },
+    getDOMstrings : function(){
+      return DOMstrings;
+    }
+  }
 
 }());
 
+// CONTROLADOR GLOBAL 
 var controller = (function(budgetCtrl, UICtrl){
 
-  var z = budgetCtrl.publicTest(2);
+  //Obtiene el objeto DOMstrings del CONTROLADOR UI
+  var DOM = UICtrl.getDOMstrings();
 
-  return {
-    anotherPublic : function(){
-      return z;
-    }
+  var ctrlAddItem = function(){
+    
+    // Obtener los datos de los campos de la UI
+    var input = UICtrl.getInput();
+    console.log(input);
   }
+
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+  
+  document.addEventListener('keypress', function(event){
+    if (event.keyCode === 13 || event.which === 13 ) {
+      ctrlAddItem();
+    }
+  })
+
 }(budgetController, UIController));
