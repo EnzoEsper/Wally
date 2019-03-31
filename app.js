c5d370c65dc07afe7dd6f4d26e3905968338a1fc
enzoesper <enzoesper@hotmail.com>
2019-03-31 17:48:07 -0300

    Actualizar porcentajes items expense en la UI

--- app.js
+++ app.js
@@ -157,7 +157,8 @@ var UIController = (function(){
     incomeLabel: '.budget__income--value',
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
-    container: '.container'
+    container: '.container',
+    expensesPercLabel: '.item__percentage'
   }
 
   return {
@@ -230,6 +231,24 @@ var UIController = (function(){
       }
     },
 
+    displayPercentages : function(percentages){
+      var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+      var nodeListForEach = function(list, callback){
+        for (var i = 0; i < list.length; i++) {
+          callback(list[i], i);
+        }
+      };
+
+      nodeListForEach(fields, function(current ,index){
+        if (percentages[index] > 0) {
+          current.textContent = percentages[index] + "%";
+        } else {
+          current.textContent = "---";
+        }
+      })
+    },
+
     getDOMstrings : function(){
       return DOMstrings;
     }
@@ -276,7 +295,7 @@ var controller = (function(budgetCtrl, UICtrl){
     // 2. Obtener los porcentajes del budget controller
     var percentages = budgetCtrl.getPercentages();
     // 3. Actualizar la UI con los nuevos porcentajes
-    console.log(percentages);
+    UICtrl.displayPercentages(percentages);
   };
 
   var ctrlAddItem = function(){
